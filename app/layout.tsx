04600ab0f26e367089369299d210b68f9df0eb46
warmice71 <awl19950228@gmail.com>
2023-12-30 14:47:08 +0100

    chatbot

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Shooketh – AI bot fine-tuned on Shakespeare's literary works",
+  description:
+    "Shooketh is an AI bot built with the Vercel AI SDK and fine-tuned on Shakespeare's literary works."
+}
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}
